f82676011d33b082b19cce5d444b6ea82fafbc41
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2020-10-10 14:05:45 +0200

    Rename setting DisableHelpFlags => DisableHelpFlag

--- tests/help.rs
+++ tests/help.rs
@@ -1843,7 +1843,7 @@ This is after help.
 fn after_help_no_args() {
     let mut app = App::new("myapp")
         .version("1.0")
-        .setting(AppSettings::DisableHelpFlags)
+        .setting(AppSettings::DisableHelpFlag)
         .setting(AppSettings::DisableVersion)
         .after_help("This is after help.");
 
